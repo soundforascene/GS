845084b66fc7719c65b3ed54c274492771137487
Curtis Rae <vardus21@gmail.com>
2019-02-06 02:48:26 +0000

    Trying webgl and resize

--- sketch.js
+++ sketch.js
@@ -1,9 +1,50 @@
-// Functions
+// Declerations 
 
-function elip(x, y){
-	fill (255,150,200);
-	ellipse (x, y, 200, 200);
+var washSong;
+var playing = false;
+let img;
+// cnv = centreing variable
+var cnv 
+
+function preload(){
+
+	washSong = loadSound('assets/audio/wash.mp3')
+	washSong.loop();
+	washSong.playMode(untilDone);
 }
+
+function setup() {
+	
+	cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+	centerCanvas();
+	imageMode(CENTER);
+	img = loadImage('cloud.png');
+
+
+}
+
+function draw() {
+	 
+	background(0,150,200);
+	//elip(mouseX, mouseY);
+	//fill(20,20,20);
+	//washSong.pan(mouseY-200);
+	rotateX(frameCount * 0.01);
+	rotateY(frameCount * 0.01);
+	texture(img);
+	torus(100, 40);
+
+	text("Move Mouse", 170, 50);
+	text(mouseY-200, 120, 50);
+	text(playing, 250, 50);
+
+	//image(img, mouseX, mouseY);
+
+	mouseClicked;
+	
+}
+
+// Functions
 
 function mouseClicked(){
 	if (!playing) {
@@ -18,47 +59,14 @@ function mouseClicked(){
     }
 }
 
-function canvas(x, y){
-	createCanvas(x, y); 
-	// Create an equasion that allows me 
-	// to get the centre of the canvas 
-} 
+// These next two functons centre the patch and allow for resizing 
 
-// Declerations 
-
-var washSong;
-var playing = false;
-let img;
-
-function preload(){
-
-	washSong = loadSound('assets/audio/wash.mp3')
-	washSong.loop();
-	washSong.playMode(untilDone);
+function centerCanvas () {
+	var x = (windowWidth - width) / 2;
+	var y = (windowHeight - height) /2;
+	cnv.position(x , y);  
 }
 
-function setup() {
-	
-	createCanvas(1600,800);
-	imageMode(CENTER);
-	img = loadImage('cloud.png');
-
-
-}
-
-function draw() {
-	 
-	background(0,150,200);
-	//elip(mouseX, mouseY);
-	//fill(20,20,20);
-	//washSong.pan(mouseY-200);
-
-	text("Move Mouse", 170, 50);
-	text(mouseY-200, 120, 50);
-	text(playing, 250, 50);
-
-	image(img, mouseX, mouseY);
-
-	mouseClicked;
-	
+function windowResized() {
+	centerCanvas();
 }
