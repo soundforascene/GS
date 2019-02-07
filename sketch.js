20e49c7f9ca548c2d2dd7303cb3927d8f7bd05e0
Curtis Rae <vardus21@gmail.com>
2019-02-07 12:36:31 +0000

    Added functionality to create more objects

--- sketch.js
+++ sketch.js
@@ -8,7 +8,6 @@ var cnv;
 var drag = 0;
 
 function preload(){
-
 	// Preloading audio assets 
 	washSong = loadSound('assets/audio/wash.mp3')
 	washSong.loop();
@@ -30,41 +29,65 @@ function draw() {
 	 
 	// All variables for readin amp and converting it to size and rotation
 	var level = amplitude.getLevel();
-	var size = map(level, 0, 1, 50, 200);
-	var rotation = map(level, 0, 1, 0.01, 0.1);
-
-	// Mappning mouse input to a different range for shape rotation
-	var mapX = map(mouseX, 0, windowWidth, -5, 5);
-	var mapY = map(mouseY, 0, windowHeight, 20, -20);
+	var size = map(level, 0, 1, 25, 100);
 
 	// Smoothes resized images and shapes
 	smooth();
 	background(0,150,200);
 
-	//camera(0, 20, 0, CENTER, CENTER)
 
-	rotateZ(drag);
-	translate(200, 0, 0);
-	sphereMove(size, 24, 24);
+	// Spheres 
+	push();
+	sphereMove(size, 24, 24, 0, 0, 0, 0);
+	pop();
+
+	push();
+	rotateZ(drag/2);
+	sphereMove(size, 24, 24, 100, 0, 0, 2);
+	pop();
+
+	push();
+	rotateZ(drag/2.5);
+	sphereMove(size, 24, 24, 200, 0, 0, 2.5);
+	pop(); 
+
+	push();
+	rotateZ(drag/3);
+	sphereMove(size, 24, 24, 300, 0, 0, 2.5);
+	pop(); 
+
+
+
 	mouseWheel;
 	mouseClicked;	
 }
 
 // Functions
 
+// x,y,z are the radius and defenition of the sphere
+// a,b,c are the translation peramiters of the sphere 
 
-function sphereMove(x, y, z) {
+// Function to add an extra layer of translation to the initRotation function. 
+// Syntax = sphereMove(sphere radius, sphere detail X, sphere detail Y, translate x, y, z, )
+function sphereMove(x, y, z, a, b, c) {
+	push();
+	texture(img);
+	translate(a, b, c);
+	initRotation(x,y,z);
+	pop();
+}
 
+// Function to create the sphere object 
+// Syntax = initRotation(radius, detailX, detailY)
+function initRotation(x, y, z) {
 	// I use push and pop to localise the Y roation to the sphere 
 	//as it its where the centre so it doesnt get roatated on the 
 	//trassnform axis later on.
-
 	push();
 	translate(0, 0, 0);
 	rotateZ(millis() / 1000);
 	sphere(x, y, z);
 	pop();
-	texture(img);
 }
 
 function mouseClicked(){
@@ -81,8 +104,9 @@ function mouseClicked(){
 }
 
 function mouseWheel(event) {
-	drag += event.delta / 1000;
+	drag += event.delta / 500;
 }
+
 // These next two functons centre the patch and allow for resizing 
 
 function centerCanvas () {
