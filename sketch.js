0ebcbc2c7beceeb70d44307eeedbafe3d7db12d7
Curtis Rae <vardus21@gmail.com>
2019-03-28 00:15:11 +0000

    Removing Audio for now

--- sketch.js
+++ sketch.js
@@ -3,13 +3,8 @@
 
 // cnv = centreing variable
 var cnv;
-var washSong;
-var playing = false;
 var drag = 0;
 var earthSize = 0.1;
-var grow = false;
-var mAmp;
-var mRate;
 
 
 function preload(){
@@ -24,10 +19,6 @@ function preload(){
 	imgUranus = loadImage('assets/images/uranus.png');
 	imgNeptune = loadImage('assets/images/neptune.png');
 	imgPluto = loadImage('assets/images/pluto.jpeg');
-	// Preloading audio assets 
-	washSong = loadSound('assets/audio/wash.mp3')
-	washSong.loop();
-	washSong.playMode(untilDone);
 }
 
 function setup() {
@@ -36,66 +27,39 @@ function setup() {
 	centerCanvas();
 	// Image 
 	imageMode(CENTER);
-	// Audio 
-	amplitude = new p5.Amplitude();
 }
 
 function draw() {
 
-
-	function makeCamera() {
-	camera(0, 0, 800, 147, 0, 0, 0, 1, 0);
-	}
-	makeCamera();
-	rotateZ((frameCount*29.8)/12000)
-
-	// All variables for reading amp and converting it to size
-	var level = amplitude.getLevel();
-	var size = map(level, 0, 1, 25, 100);
 	// Smoothes resized images and shapes
 	smooth();
-	// nice blue background(0,150,200);
+	// Background Colour 
 	background(0, 0, 0);
-	// control for drag and zoom
-	orbitControl();
 	//Camera
+	orbitControl();
+
+	var size = map(0.02, 0, 1, 25, 100);
+
+	function makeSun(n, r, s, x, y){
+		push();
+		ambientMaterial(200,200,255);
+		texture(n);
+		rotateZ((frameCount*r)/12000);
+		sphereMove(((size*earthSize)*s)/5, x);
+		pop();
+	}
+
+	function makePlanet(n, r, s, x){
+		push();
+		// Calculate Radius 
+		rad = PI * (x*2)
+		texture(n);
+		rotateZ((frameCount*r)/4000);
+		sphereMove(((size*earthSize)*s)/5, x);
+		pop();
+	}
 	
-
-	//camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
-
-	 mAmp = map(mouseX, 0, 1439, 0, 1);
-	 mRate = map(mouseY, 0, 748, -0, 1);
-
-	 washSong.setVolume(mAmp);
-	 washSong.rate(mRate);
-	 //print(mouseY);
-
-// Syntax: n = name, r = rotation (relative speed around the sun), s = size (size compared to earth), x = x transform (distance from the sun)
-function makeSun(n, r, s, x, y){
-	push();
-	ambientMaterial(200,200,255);
-	texture(n);
-	rotateZ((frameCount*r)/12000);
-	sphereMove(((size*earthSize)*s)/5, x);
-	pop();
-}
-function makePlanet(n, r, s, x, y){
-	push();
-	if (grow == true) {
-		s *= 15;
-	}
-	// Calculate Radius 
-	rad = PI * (x*2)
-
-	texture(n);
-	rotateZ((frameCount*r)/4000);
-	sphereMove(((size*earthSize)*s)/5, x);
-	pop();
-	if (y == 1) {
-		saturnRing();
-	}
-}
-
+	// Syntax: n = name, r = rotation (relative speed around the sun), s = size (size compared to earth), x = x transform (distance from the sun)
 	makeSun(sun, 0, 109, 0);
 	makePlanet(imgMercury, 47, (0.3*20), 69);
 	makePlanet(imgVenus, 35, (0.9*20), 109);
@@ -107,20 +71,10 @@ function makePlanet(n, r, s, x, y){
 	makePlanet(imgNeptune, 5.5, (3.8*5), 900-400);
 	makePlanet(imgPluto, 4.6, (0.18*10), 1000-400);
 
-	//print((frameCount*29.8)/4000);
-
-	doubleClicked;	
 }
 
 // Functions
 
-function saturnRing() {
-	translate(375, 0, 0)
-	rotateZ ((frameCount*9.6)/4000);
-	texture(imgSaturn);
-	//for (var i = 20000; i <= 24000; i += 2000) {
-	torus(75, 1);
-}
 // Function to add an extra layer of translation to the initSphere function. 
 // Syntax: x = sphere radius, a = translate X 
 function sphereMove(x, a) {
@@ -132,6 +86,7 @@ function sphereMove(x, a) {
 
 // Function to create the sphere object 
 // Syntax = initSphere(radius, detailX, detailY)
+
 function initSphere(x) {
 	// I use push and pop to localise the Y roation to the sphere 
 	//as it its where the centre so it doesnt get roatated on the 
@@ -141,22 +96,6 @@ function initSphere(x) {
 	rotateZ(millis() / 1000);
 	sphere(x, 24, 24);
 	pop();
-}
-
-// Function turns on and off music when double clicking
-function doubleClicked(){
-	if (!playing) {
-	
-		washSong.play();
-		playing = true;
-		grow = true;
-	}
-	else {
-		washSong.setVolume(0);
-		washSong.pause();
-		playing = false;
-		grow = false;
-    }
 }
 
 // These next two functons centre the patch and allow for resizing 
